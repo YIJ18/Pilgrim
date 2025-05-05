606e268196e9e6a86be2008030546b1d6fe751ad
YIJ18 <jassoiris96@gmail.com>
2025-05-04 18:03:26 -0600

    inicio_sesion

---
+++ Pilgrim_inicio/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'pilgrin-8af06',
+    appId: '1:935241045802:web:e665a0393cca70fbc57053',
+    storageBucket: 'pilgrin-8af06.firebasestorage.app',
+    apiKey: 'AIzaSyBDYIF20o8nNeHrHJhRct2RdPdrhlBwyeU',
+    authDomain: 'pilgrin-8af06.firebaseapp.com',
+    messagingSenderId: '935241045802',
+    measurementId: 'G-02LX2GP1EQ', // este campo es opcional, solo si lo usas con analytics
+  },
+};
